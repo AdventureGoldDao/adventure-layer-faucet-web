82cd924816f47914ac432aafb44c09b14527b88c
Jason <zhiyuan.liang6@gmail.com>
2025-05-06 08:52:21 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     faucetUrl: "https://faucet-testnet.adventurelayer.xyz",
     bridgeUrl: "https://bridge-testnet.adventurelayer.xyz",
     docsUrl: "https://docs.adventurelayer.xyz",
-    rpcUrl: "https://rpc-testnet.adventurelayer.xyz/2dc324be41f455072e67fc05138a7312b6e7dcdf",
+    rpcUrl: "https://rpc-testnet.adventurelayer.xyz",
     turnstileSiteKey: "0x4AAAAAABaAe_m-eq0l4T6Y"
 };
 
@@ -10,8 +10,8 @@ export default config;
 
 export const AdventureLayer = {
   chainId: 242070,
-  rpcUrl: "https://rpc-testnet.adventurelayer.xyz/2dc324be41f455072e67fc05138a7312b6e7dcdf",
-  wssUrl: "wss://rpc-testnet.adventurelayer.xyz/2dc324be41f455072e67fc05138a7312b6e7dcdf",
+  rpcUrl: "https://rpc-testnet.adventurelayer.xyz",
+  wssUrl: "wss://rpc-testnet.adventurelayer.xyz",
   chainName: 'Adventure Layer L2',
   blockExplorerUrl: 'https://explorer-testnet.adventurelayer.xyz',
 }
